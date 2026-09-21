bdf3c8231f6abc2e0209c1e89473ea2d162df2b5
harinireddylingala56 <harinireddylingala56@gmail.com>
2026-09-21 14:36:45 +0530

    second commit

--- routes/emp_route.js
+++ routes/emp_route.js
@@ -1,19 +1,42 @@
 let express = require("express");
 let router = express.Router();
+let bcrypt = require("bcrypt");
 let { users } = require("../models/users");
 router.post("/register", async (req, res) => {
     console.log(req.body);
+    req.body.password = await bcrypt.hash(req.body.password, 10);
     let newuser = users(req.body);
     let result = await newuser.save();
     res.send(result);
 })
-router.post("/login", (req, res) => {
-    res.send("login page called");
+router.post("/login", async (req, res) => {
+    let result = await users.findOne({ email: req.body.email })
+    // result.password = undefined;//skip the passworg and give next features
+    if (result) {
+        let matchpass = await bcrypt.compare(req.body.password, result.password);
+        if (matchpass) {
+            res.send("login successfully");
+        } else {
+            res.send("login failed");
+        }
+    } else {
+        res.send("user not found");
+    }
+    res.send(result);
 })
 router.post("/view task", (req, res) => {
     res.send("view task  page called");
 })
 router.post("/updatestatus", (req, res) => {
     res.send("updatestatus page called");
+})
+router.patch("/updateprofile/:id", async (req, res) => {
+    let data = req.body;
+    if (data.password) {
+        data.password = await bcrypt.hash(data.password, 10);
+    }
+    let result = await users.findByIdAndUpdate(req.params.id, data, { new: true });
+    res.send(result);
+
 })
 module.exports = router;
--- routes/hr_route.js
+++ routes/hr_route.js
@@ -1,16 +1,34 @@
 let express = require("express");
 let router = express.Router();
-router.post("/register", (req, res) => {
-    console.log(req.body);
-    res.send("register page called");
+let { users } = require('../models/users')
+router.get("/viewemployees", async (req, res) => {
+    let result = await users.find();
+    res.send(result);
+});
+router.post("/assignemployees", (req, res) => {
+    res.send("Assign Employees router");
 })
-router.post("/login", (req, res) => {
-    res.send("login page called");
-})
-router.post("/view task", (req, res) => {
-    res.send("view task  page called");
-})
-router.post("/updatestatus", (req, res) => {
-    res.send("updatestatus page called");
+router.delete("/deleteemployees/:id", async (req, res) => {
+    let result = await users.findByIdAndDelete(req.params.id)
+    if (result) {
+        res.send("employee deleted success");
+    }
+    else {
+        res.send("no user found");
+    }
 })
 module.exports = router;
+// router.post("/register", (req, res) => {
+//     console.log(req.body);
+//     res.send("register page called");
+// })
+// router.post("/login", (req, res) => {
+//     res.send("login page called");
+// })
+// router.post("/view task", (req, res) => {
+//     res.send("view task  page called");
+// })
+// router.post("/updatestatus", (req, res) => {
+//     res.send("updatestatus page called");
+// })
+//module.exports = router;
